7ead5a925d88e83b3ca45e5b1bc8ced860496eec
Siripoom Limsoontorn <110332113+poommer@users.noreply.github.com>
2025-03-31 11:41:27 +0700

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,71 +1,11 @@
-import * as React from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-  createRoutesFromElements,
-} from "react-router-dom";
-import { Layout } from "./layout/Layout";
-import { Homepage } from './routes/homepage/Homepage';
-import { Detail, Lesson } from "./routes/lesson/Lesson";
-import { WordSearchPage } from "./routes/lesson/word-search";
-
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <>
-       <Layout />
-      </>
-    ),
-    children: [
-      {
-        path: "/",
-        element: (
-          <div>
-           { localStorage.getItem('user') ? <Lesson /> : <Homepage />}
-          </div>
-        ),
-      },
-      {
-        path: "/lesson/:type/:lesson/:quiz",
-        element: (
-          <div>
-            <Detail />
-          </div>
-        ),
-      },
-      {
-        path: "/lesson/word-search/:lesson/:quiz",
-        element: (
-          <div>
-            <WordSearchPage />
-          </div>
-        ),
-      },
-    ]
-  }
-]
-  // createRoutesFromElements(
-  //   <Route >
-  //     <Route path="/"  element={<Layout />} >
-  //       <Route path="/" element={localStorage.getItem('user') ? <Lesson /> : <Homepage />} />
-  //       <Route path="/lesson/:type/:lesson/:quiz" element={<Detail />} />
-  //       <Route path="/lesson/word-search/:lesson/:quiz" element={<WordSearchPage />} />
-  //     </Route>
-
-  //   </Route>
-
-  // )
-);
+// import App from "./App.jsx";
+import "./index.css";
+import App from './App';
 
 createRoot(document.getElementById("root")).render(
-  <>
-    <RouterProvider router={router} />
-  </>
-
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
